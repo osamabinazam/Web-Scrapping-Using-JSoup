8c07555c3126b1cfa05a8e4cf2e363cd5a2ff084
Osama Bin Azam <osamabinazam@gmail.com>
2023-01-06 12:32:14 +0500

    Some Changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Calling Scrapping so that GUI Pop up to User
         new Scrapping();
     }
 }
